04b507c1459cd62dee15ed291baf83d6d1f645bb
Tema Popov <artpop92@gmail.com>
2011-03-17 21:52:52 +0300

    добавлен SQL файл

--- sql/patch0.sql
+++ sql/patch0.sql
@@ -1,11 +1,19 @@
-CREATE DATABASE  'jobs' ;
+CREATE DATABASE  jobs CHARACTER SET 'UTF8';
 
-CREATE TABLE  'jobs'.'all_jobs' (
-'title' TEXT NOT NULL ,
-'description' TEXT NOT NULL ,
-'link' TEXT NOT NULL ,
-'salary' TEXT NOT NULL ,
-'full_day' BOOL NOT NULL
-'cl'
+CREATE TABLE  jobs.all_jobs (
+title TEXT NOT NULL ,
+description TEXT NOT NULL ,
+link varchar(255) NOT NULL ,
+salary TEXT NOT NULL ,
+full_day BOOL NOT NULL ,
+cluster_id INT DEFAULT '0' ,
+ UNIQUE (link)
+) CHARACTER SET 'UTF8';
 
-)
+CREATE TABLE  jobs.var (
+var_name varchar(255) NOT NULL ,
+var_value INT DEFAULT 0 ,
+ UNIQUE (var_name)
+) CHARACTER SET utf8;
+
+INSERT INTO jobs.var VALUES('last_cluster', '0');
